c1bcd2376c8bf84529bec0433d9f8d4d3103fda2
sanam2002usa@gmail.com <sanam2002usa@gmail.com>
2018-09-09 02:49:26 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,7 @@
 
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
-    expect(shout('hello')).toEqual('HELLO')
+    expect(shout('hello')).toEqual('HELL0')
   })
 })
 
@@ -37,7 +37,7 @@ describe('logWhisper(string)', function() {
 
 describe('sayHiToGrandma(string)', function() {
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+    expect(sayHiToGrandma('hello')).toEqual("I can\'t hear you!")
   })
 
   it('returns "YES INDEED!" if `string` is uppercase', function() {
